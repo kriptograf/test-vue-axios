81230eb76a10a72de46d1d6a1f52e7f09c8da94a
foreach <foreach@mail.ru>
2019-04-19 15:06:34 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Bitcoin Price Index</h2>
+    <div v-for="currency in info" :key="currency.id" class="currency">
+        <span class="desc">{{ currency.description }}</span>
+        <span class="lighten">
+          <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+        </span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/**
+ * Импортируем библиотеку axios для получения данных с удаленного сервера
+ */
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      info: null /* Определяем переменную, куда будем складывать ответ сервера */
+    };
+  },
+  mounted(){
+    /**
+     * Запрашиваем данные с удаленного сервера и присваиваем в переменную info
+     * Вызывается сразу после монтирования экземпляра, когда взамен el создан
+     */
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => (this.info = response.data.bpi));
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
   }
 }
 </script>
 
 <style>
+body{
+  padding-top: 60px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 0 auto;
+  width: auto;
+  padding: 30px;
+  background: #2F242C;
+  color: #B3BFB8;
+  width: 400px;
+  border-radius: 10px;
+}
+h2{
+  text-align: center;
+}
+.lighten {
+    color: white;
+}
+.desc{
+  width: 250px;
+  display: inline-block;
 }
 </style>
